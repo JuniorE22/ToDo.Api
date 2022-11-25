4c980af3ce7046357e25432f889c3d24ccff6d4d
Jhonny <juniol2229@gmail.com>
2022-11-24 20:43:54 -0600

    metodos contructores en User

--- ToDo.Model/Entities/User.cs
+++ ToDo.Model/Entities/User.cs
@@ -7,6 +7,23 @@
         public string UserName { get; set; }
         public string Password { get; set; }
 
+
+        public User()
+        {
+
+        }
+        public User(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+        public User(string name, string username, string password)
+        {
+            Name = name;
+            UserName = username;
+            Password = password;
+        }
+
     }
 
 }
