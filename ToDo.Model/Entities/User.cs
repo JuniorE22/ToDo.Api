3dd0969719291443b7d23705169585abe3d36215
Jhonny <juniol2229@gmail.com>
2022-11-24 20:46:57 -0600

    añadir metodos contructores a User

--- ToDo.Model/Entities/User.cs
+++ ToDo.Model/Entities/User.cs
@@ -2,5 +2,26 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+        public User()
+        {
+
+        }
+        public User(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+        public User(string name, string username, string password)
+        {
+            Name = name;
+            UserName = username;
+            Password = password;
+        }
     }
 }
