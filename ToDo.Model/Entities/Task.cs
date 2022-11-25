07f56973ded687fa60532b9b1305e2e9e94f2eb4
Jhonny <juniol2229@gmail.com>
2022-11-24 21:23:01 -0600

    añadi metodo contructores en Task

--- ToDo.Model/Entities/Task.cs
+++ ToDo.Model/Entities/Task.cs
@@ -4,6 +4,16 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public  string Description { get; set; }
 
+        public Task()
+        {
+
+        }
+        public Task(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
     }
 }
