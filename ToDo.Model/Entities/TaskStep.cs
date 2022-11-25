4896d4b150213a33dae3ccb58f63c0095a759ca8
Jhonny <juniol2229@gmail.com>
2022-11-24 21:02:48 -0600

    añadi contructores a TaskStep

--- ToDo.Model/Entities/TaskStep.cs
+++ ToDo.Model/Entities/TaskStep.cs
@@ -5,5 +5,15 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public TaskStep()
+        {
+
+        }
+        public TaskStep(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
     }
 }
