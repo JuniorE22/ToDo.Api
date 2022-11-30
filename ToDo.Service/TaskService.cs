17925cc695f708c2e47d7f415404583d149d744b
Jhonny <juniol2229@gmail.com>
2022-11-29 18:10:36 -0600

    Implemente IServiceBa en TaskService

---
+++ ToDo.Service/TaskService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApi.Services;
+
+namespace ToDo.Service
+{
+    internal class TaskService : IServiceBase<Task>
+    {
+    }
+}
